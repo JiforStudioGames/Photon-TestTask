24ad05c12a95bc1932e4f8099d099cd3d0e1ea43
Victor Podberezkin <125683179+JiforStudioGames@users.noreply.github.com>
2026-04-07 19:19:27 +0300

    [UPDATE]

--- Assets/Scripts/Installers/GameInstaller.cs
+++ Assets/Scripts/Installers/GameInstaller.cs
@@ -4,7 +4,7 @@ public class GameInstaller : Installer<GameInstaller>
 {
     public override void InstallBindings()
     {
-        Container.BindInterfacesTo<PlayerService>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<PlayerService>().AsSingle().NonLazy();
         Container.BindInterfacesTo<PlayerPresenter>().AsSingle().NonLazy();
         Container.BindInterfacesTo<MedkitPresenter>().AsSingle().NonLazy();
     }
